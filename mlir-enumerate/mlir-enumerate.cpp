906dff85a0822f134a8d2f83fe4a0c89ad5c15fc
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-09-08 03:02:06 +0200

    Add snippet to create a function

--- mlir-enumerate/mlir-enumerate.cpp
+++ mlir-enumerate/mlir-enumerate.cpp
@@ -7,9 +7,46 @@
 //===----------------------------------------------------------------------===//
 
 #include "Dyn/Dialect/IRDL/IR/IRDL.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
 
-using namespace llvm;
 using namespace mlir;
 using namespace irdl;
 
-int main(int argc, char **argv) { llvm::errs() << "Hello World\n"; }
+OwningOpRef<ModuleOp> createProgram(MLIRContext &ctx) {
+  // Create an empty module.
+  auto unknownLoc = UnknownLoc::get(&ctx);
+  OwningOpRef<ModuleOp> module(ModuleOp::create(unknownLoc));
+
+  // Create the builder, and set its insertion point in the module.
+  OpBuilder builder(&ctx);
+  auto &moduleBlock = module->getRegion().getBlocks().front();
+  builder.setInsertionPoint(&moduleBlock, moduleBlock.begin());
+
+  // Create an empty function, and set the insertion point in it.
+  auto func = builder.create<func::FuncOp>(unknownLoc, "foo",
+                                           FunctionType::get(&ctx, {}, {}));
+  func.setPrivate();
+  auto &funcBlock = func.getBody().emplaceBlock();
+  builder.setInsertionPoint(&funcBlock, funcBlock.begin());
+
+  builder.create<func::ReturnOp>(unknownLoc);
+  return module;
+}
+
+int main(int argc, char **argv) {
+  MLIRContext ctx;
+
+  // Register all dialects
+  DialectRegistry registry;
+  registerAllDialects(registry);
+  ctx.appendDialectRegistry(registry);
+  ctx.getOrLoadDialect<irdl::IRDLDialect>();
+  ctx.loadAllAvailableDialects();
+
+  OwningOpRef<ModuleOp> module = createProgram(ctx);
+
+  module->print(llvm::outs());
+}
