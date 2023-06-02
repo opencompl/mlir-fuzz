c70acc1ab705c53659d34d4e39b996b5e83a1618
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-06-02 20:16:14 +0100

    Generate a lot of random bad programs

--- experimental/comb-fuzzer/comb-fuzzer.cpp
+++ experimental/comb-fuzzer/comb-fuzzer.cpp
@@ -16,12 +16,53 @@
 
 using namespace mlir;
 
-/// Create a value of the given type.
-/// This may add a new argument to the function.
-Value createValue(GeneratorInfo &info, int fuel, Type type) {
-  if (fuel == 0)
-    return getValue(info, type);
-  assert(false && "Not implemented");
+constexpr int MAX_WIDTH = 6;
+std::string binaryOps[] = {"comb.divu", "comb.divs", "comb.modu", "comb.mods",
+                           "comb.shl",  "comb.shru", "comb.shrs", "comb.sub"};
+
+Value addBinary(GeneratorInfo &info) {
+  std::vector<Type> types;
+  for (auto val : info.dominatingValues) {
+    if (val.second.size() > 1) {
+      types.push_back(val.first);
+    }
+  }
+
+  assert(!types.empty() && "Not enough values to create a binary operation");
+
+  auto type = types[info.chooser->choose(types.size())];
+  auto op_name = binaryOps[info.chooser->choose(8)];
+  auto lhs = info.dominatingValues[type][info.chooser->choose(
+      info.dominatingValues[type].size())];
+  auto rhs = info.dominatingValues[type][info.chooser->choose(
+      info.dominatingValues[type].size())];
+
+  auto ctx = info.builder.getContext();
+  auto *operation =
+      info.builder.create(UnknownLoc::get(ctx), StringAttr::get(ctx, op_name),
+                          {lhs, rhs}, {lhs.getType()});
+  info.addDominatingValue(operation->getResult(0));
+  return operation->getResult(0);
+}
+
+/// Add an operation
+Value addOperation(GeneratorInfo &info) {
+  bool canUseBinary = false;
+  for (auto val : info.dominatingValues) {
+    if (val.second.size() > 1) {
+      canUseBinary = true;
+      break;
+    }
+  }
+
+  bool hasi1 = info.dominatingValues.find(
+                   IntegerType::get(info.builder.getContext(), 1)) !=
+               info.dominatingValues.end();
+
+  // if (info.chooser->choose(2)) {
+  return addBinary(info);
+  //}
+  // assert(false && "Not implemented");
 }
 
 OwningOpRef<ModuleOp> createProgram(MLIRContext &ctx,
@@ -44,8 +85,24 @@ OwningOpRef<ModuleOp> createProgram(MLIRContext &ctx,
 
   GeneratorInfo info(chooser, {}, builder,
                      ctx.getOrLoadDialect<irdl::IRDLDialect>()->irdlContext);
+
+  // Add arguments to the function.
+  func.insertArgument(0, IntegerType::get(&ctx, 1), {},
+                      mlir::UnknownLoc::get(builder.getContext()));
+  info.addDominatingValue(func.getArgument(0));
+
+  // Add arguments to the function.
+  func.insertArgument(1, IntegerType::get(&ctx, 1), {},
+                      mlir::UnknownLoc::get(builder.getContext()));
+  info.addDominatingValue(func.getArgument(1));
+
+  // Add arguments to the function.
+  func.insertArgument(2, IntegerType::get(&ctx, 1), {},
+                      mlir::UnknownLoc::get(builder.getContext()));
+  info.addDominatingValue(func.getArgument(2));
+
   for (int i = 0; i < fuel; i++) {
-    createValue(info, 0, IntegerType::get(&ctx, chooser->choose(7)));
+    addOperation(info);
   }
 
   builder.create<func::ReturnOp>(unknownLoc);
@@ -62,6 +119,7 @@ int main(int argc, char **argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv, "MLIR enumerator");
 
   MLIRContext ctx;
+  ctx.allowUnregisteredDialects();
 
   // Register all dialects
   DialectRegistry registry;
