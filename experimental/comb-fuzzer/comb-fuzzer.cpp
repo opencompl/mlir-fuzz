c7711ab3eac9b8cb8c6eb35932159603cf31576d
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-06-12 14:04:42 +0100

    Add variadic operations

--- experimental/comb-fuzzer/comb-fuzzer.cpp
+++ experimental/comb-fuzzer/comb-fuzzer.cpp
@@ -19,6 +19,8 @@ using namespace mlir;
 constexpr int MAX_WIDTH = 6;
 std::string binaryOps[] = {"comb.divu", "comb.divs", "comb.modu", "comb.mods",
                            "comb.shl",  "comb.shru", "comb.shrs", "comb.sub"};
+std::string variadicOps[] = {"comb.add", "comb.mul", "comb.and", "comb.or",
+                             "comb.xor"};
 
 Value addBinary(GeneratorInfo &info) {
   std::vector<Type> types;
@@ -49,6 +51,36 @@ Value addBinary(GeneratorInfo &info) {
   return operation->getResult(0);
 }
 
+Value addVariadic(GeneratorInfo &info) {
+  std::vector<Type> types;
+  for (auto val : info.dominatingValues) {
+    if (val.second.size() > 1) {
+      types.push_back(val.first);
+    }
+  }
+
+  assert(!types.empty() && "Not enough values to create a binary operation");
+
+  // TODO: Handle variadic operations with more than 2 operands
+  auto type = types[info.chooser->choose(types.size())];
+  auto op_name = variadicOps[info.chooser->choose(5)];
+  auto lhs = info.dominatingValues[type][info.chooser->choose(
+      info.dominatingValues[type].size())];
+  auto rhs = info.dominatingValues[type][info.chooser->choose(
+      info.dominatingValues[type].size())];
+
+  if (lhs == rhs &&
+      !(op_name == "comb.and" || op_name == "comb.or" || op_name == "comb.xor"))
+    return nullptr;
+
+  auto ctx = info.builder.getContext();
+  auto *operation =
+      info.builder.create(UnknownLoc::get(ctx), StringAttr::get(ctx, op_name),
+                          {lhs, rhs}, {lhs.getType()});
+  info.addDominatingValue(operation->getResult(0));
+  return operation->getResult(0);
+}
+
 /// Add an operation
 Value addOperation(GeneratorInfo &info) {
   bool canUseBinary = false;
@@ -63,9 +95,11 @@ Value addOperation(GeneratorInfo &info) {
                    IntegerType::get(info.builder.getContext(), 1)) !=
                info.dominatingValues.end();
 
-  // if (info.chooser->choose(2)) {
-  return addBinary(info);
-  //}
+  if (info.chooser->choose(2)) {
+    return addBinary(info);
+  } else {
+    return addVariadic(info);
+  }
   // assert(false && "Not implemented");
 }
 
