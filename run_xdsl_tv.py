82ef0d140a2db388dfc9515ef2ae109416cf88ee
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-11-14 01:40:53 +0000

    Run tests in parallel

--- run_xdsl_tv.py
+++ run_xdsl_tv.py
@@ -1,26 +1,33 @@
 """Generate random programs, use the canonicalizer, and run xdsl-tv on it."""
 
 import subprocess
+import threading
 
 num_ran = 0
 
 
-def run_once() -> None:
+def run_once(id: int) -> None:
     res = subprocess.run(
-        "./build/bin/mlir-enumerate dialects/arith.mlir | mlir-opt --mlir-print-op-generic -o test1.mlir && mlir-opt --canonicalize --mlir-print-op-generic test1.mlir -o test2.mlir && xdsl-tv test1.mlir test2.mlir | z3 -in",
+        f"./build/bin/mlir-enumerate dialects/arith.mlir | mlir-opt --mlir-print-op-generic -o tmp/test{id}.mlir && mlir-opt --canonicalize --mlir-print-op-generic tmp/test{id}.mlir -o tmp/test{id}-opt.mlir && xdsl-tv tmp/test{id}.mlir tmp/test{id}-opt.mlir | z3 -in",
         capture_output=True,
         text=True,
         shell=True,
     )
     if res.returncode != 0:
-        raise Exception("Process failed")
+        raise Exception(f"Process {id} failed: " + res.stderr)
 
     if "unsat" not in res.stdout:
-        raise Exception("Failed translation validation")
+        raise Exception(f"Process {id} failed translation validation")
+
     global num_ran
     num_ran += 1
     print(f"{num_ran} succeeded")
 
 
-while True:
-    run_once()
+def run_indifinitely(id: int) -> None:
+    while True:
+        run_once(id)
+
+
+for i in range(20):
+    threading.Thread(target=run_indifinitely, args=[i]).start()
