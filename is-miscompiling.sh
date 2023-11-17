8a2a3d1895481b589983d0c74643ad2927a753a5
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-11-17 01:44:34 +0000

    Check another pass

--- is-miscompiling.sh
+++ is-miscompiling.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mlir-opt $1 -o $1.before.tmp --mlir-print-op-generic
-mlir-opt $1.before.tmp --canonicalize --mlir-print-op-generic -o $1.after.tmp
+mlir-opt $1.before.tmp --arith-expand --mlir-print-op-generic -o $1.after.tmp
 xdsl-tv $1.before.tmp $1.after.tmp | z3 -in > $1.z3res.tmp
 ! grep -q unsat $1.z3res.tmp
