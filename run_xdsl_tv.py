0b99e37d30da90338c87fd6f250d3210f73a6bb6
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-11-17 16:06:26 +0000

    Add timeout, and use the miscompiling script

--- run_xdsl_tv.py
+++ run_xdsl_tv.py
@@ -13,25 +13,32 @@ def run_once(id: int) -> None:
     global total_time
     start = time.time()
     res = subprocess.run(
-        f"./build/bin/mlir-enumerate dialects/arith.mlir | mlir-opt --mlir-print-op-generic -o tmp/test{id}.mlir && mlir-opt --arith-expand --mlir-print-op-generic tmp/test{id}.mlir -o tmp/test{id}-opt.mlir && xdsl-tv tmp/test{id}.mlir tmp/test{id}-opt.mlir | z3 -in",
+        f"./build/bin/mlir-enumerate dialects/arith.mlir | mlir-opt -o tmp/test{id}.mlir && ./is-miscompiling.sh tmp/test{id}.mlir",
         capture_output=True,
         text=True,
         shell=True,
     )
     end = time.time()
     total_time += end - start
-    if res.returncode != 0:
-        raise Exception(f"Process {id} failed: " + res.stderr)
-
-    if "unsat" not in res.stdout:
-        subprocess.run(
-            f"mv tmp/test{id}.mlir miscompilations/test{num_ran}.mlir", shell=True
-        )
-        print(f"Found miscompilation {num_ran}!")
+    if res.returncode == 0:
+        with open(f"tmp/test{id}.mlir.z3res.tmp", "r") as f:
+            res = f.read()
+            if "sat" in res:
+                subprocess.run(
+                    f"mv tmp/test{id}.mlir miscompilations/test{num_ran}.mlir", shell=True
+                )
+                print(f"Found miscompilation {num_ran}!")
+            else:
+                assert "unknown" in res
+                
+                subprocess.run(
+                    f"mv tmp/test{id}.mlir timeouts/test{num_ran}.mlir", shell=True
+                )
+                print(f"Found timeout {num_ran}!")
 
     num_ran += 1
     if num_ran % 10 == 0:
-        print(f"{num_ran} succeeded")
+        print(f"{num_ran} ran")
         print(f"Average time: {total_time / num_ran}")
 
 
