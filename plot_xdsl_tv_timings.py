c98135e03ebaefca23cc4e741d3f36e2e09125bd
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-02-22 14:02:33 +0000

    Improve plot

--- plot_xdsl_tv_timings.py
+++ plot_xdsl_tv_timings.py
@@ -9,13 +9,16 @@ print(sum(x[0][1] for x in timings))
 print(sum(x[1][1] for x in timings))
 
 # Sort by z3 time on unoptimized version
-timings.sort(key=lambda x: x[1][1])
+timings.sort(key=lambda x: x[0][1])
 
 z3_times_with_opt = [x[0][1] for x in timings]
 z3_times_without_opt = [x[1][1] for x in timings]
 
-plt.scatter(range(len(timings)), z3_times_with_opt, label="With optimization")
-plt.scatter(range(len(timings)), z3_times_without_opt, label="Without optimization")
+# Make the dots small
+plt.scatter(range(len(timings)), z3_times_with_opt, label="With optimization", s=1)
+plt.scatter(
+    range(len(timings)), z3_times_without_opt, label="Without optimization", s=1
+)
 plt.xlabel("Test case")
 plt.ylabel("Time in seconds")
 plt.legend()
